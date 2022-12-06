b68e3580119869bbbed40206f460e1936ad09111
Sherif-ElGeziry <118230210+Sherif-ElGeziry@users.noreply.github.com>
2022-12-06 22:01:10 +0200

    Injecting literal values using basketball class

--- 01 SpringDemo/src/com/luv2code/springdemo/BasketballCoach.java
+++ 01 SpringDemo/src/com/luv2code/springdemo/BasketballCoach.java
@@ -5,13 +5,36 @@ public class BasketballCoach implements Coach {
 	// define a private field for the dependency
 	private FortuneService fortuneService;
 
+	// add new fields for email address and team
+	private String emailAddress;
+	private String team;
+
 	public BasketballCoach() {
 		System.out.println("BasketballCoach: inside no-arg constructor");
 	}
 
-	// our setter method
+	// our setter methods
 	public void setFortuneService(FortuneService fortuneService) {
+		System.out.println("BasketballCoach: inside setter method - setFortuneService");
 		this.fortuneService = fortuneService;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		System.out.println("BasketballCoach: inside setter method - setEmailAddress");
+		this.emailAddress = emailAddress;
+	}
+
+	public void setTeam(String team) {
+		System.out.println("BasketballCoach: inside setter method - setTeam");
+		this.team = team;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public String getTeam() {
+		return team;
 	}
 
 	@Override
