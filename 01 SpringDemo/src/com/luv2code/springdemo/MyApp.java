e8b36ba07b8ce7e132a953db97c6da142ae320cb
Sherif-ElGeziry <118230210+Sherif-ElGeziry@users.noreply.github.com>
2022-12-06 03:58:15 +0200

    Test

--- 01 SpringDemo/src/com/luv2code/springdemo/MyApp.java
+++ 01 SpringDemo/src/com/luv2code/springdemo/MyApp.java
@@ -3,6 +3,7 @@ package com.luv2code.springdemo;
 public class MyApp {
 
 	public static void main(String[] args) {
+
 		// create the object
 		Coach theCoach1 = new BaseballCoach();
 		Coach theCoach2 = new TrackCoach();
