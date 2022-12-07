ccee907cab82e0264e28322bc97e5de7671ffada
Sherif-ElGeziry <118230210+Sherif-ElGeziry@users.noreply.github.com>
2022-12-07 03:42:41 +0200

    Annotation tested with default bean id

--- 02 SpringDemoAnnotations/src/com/luv2code/springdemo/TennisCoach.java
+++ 02 SpringDemoAnnotations/src/com/luv2code/springdemo/TennisCoach.java
@@ -2,7 +2,8 @@ package com.luv2code.springdemo;
 
 import org.springframework.stereotype.Component;
 
-@Component("thatSillyCoach")
+// @Component("thatSillyCoach") // removed explicit bean id and let default name instead "tennisCoach" (lower case first letter)
+@Component
 public class TennisCoach implements Coach {
 
 	@Override
