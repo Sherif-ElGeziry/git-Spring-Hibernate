bfec74259ef47b14aeff7e6e2786f035f8ddac9a
Sherif-ElGeziry <118230210+Sherif-ElGeziry@users.noreply.github.com>
2022-12-07 05:50:34 +0200

    Incomplete reading fortunes from a file

--- 02 SpringDemoAnnotations/src/com/luv2code/springdemo/RandomFortuneService.java
+++ 02 SpringDemoAnnotations/src/com/luv2code/springdemo/RandomFortuneService.java
@@ -1,5 +1,11 @@
 package com.luv2code.springdemo;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import org.springframework.stereotype.Component;
@@ -8,19 +14,41 @@ import org.springframework.stereotype.Component;
 public class RandomFortuneService implements FortuneService {
 
 	// create an array of strings
-	private String[] randomFortune = { "Beware of the wolf in sheep's clothing", "Diligence is the mother of good luck",
-			"The journey is the reward" };
+	private String fileName = "C:/Users/scs/git/git-Spring-Hibernate/02 SpringDemoAnnotations/src/fortuneData.txt";
+	private List<String> theFortunes;
 
 	// create a random number generator
 	private Random myRandom = new Random();
 
+	public RandomFortuneService() {
+		File theFile = new File(fileName);
+
+		System.out.println("Reading fortunes from file: " + theFile);
+		System.out.println("File exists: " + theFile.exists());
+
+		// initialize arrayList
+		theFortunes = new ArrayList<String>();
+
+		// read fortunes from file
+		try (BufferedReader br = new BufferedReader(new FileReader(theFile))) {
+
+			String tempLine;
+			while ((tempLine = br.readLine()) != null) {
+				theFortunes.add(tempLine);
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
 	@Override
 	public String getFortune() {
 		// pick a random string from the array
-		int index = myRandom.nextInt(randomFortune.length);
+		int index = myRandom.nextInt(theFortunes.size());
 
-		String theFortune = randomFortune[index];
-		return theFortune;
+		String tempFortune = theFortunes.get(index);
+		return tempFortune;
 	}
 
 }
