0eab0a7f1060ca1de51b70a6fa0f1ec5a83fb35e
Sherif-ElGeziry <118230210+Sherif-ElGeziry@users.noreply.github.com>
2022-12-11 23:45:15 +0200

    Define Spring bean s with java code (no xml)

--- 02 SpringDemoAnnotations/src/com/luv2code/springdemo/SportConfig.java
+++ 02 SpringDemoAnnotations/src/com/luv2code/springdemo/SportConfig.java
@@ -1,10 +1,21 @@
 package com.luv2code.springdemo;
 
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("com.luv2code.springdemo")
+//@ComponentScan("com.luv2code.springdemo")
 public class SportConfig {
 
+	// define bean for our sad fortune service
+	@Bean
+	public FortuneService saFortuneService() {
+		return new SadFortuneService();
+	}
+
+	// define bean for out swimCoach and inject dependency
+	@Bean
+	public Coach swimCoach() {
+		return new SwimCoach(saFortuneService());
+	}
 }
