a8a2847f6c133aa0d0e3ce53d303c0b6e82de063
Sherif-ElGeziry <118230210+Sherif-ElGeziry@users.noreply.github.com>
2022-12-15 01:33:22 +0200

    Added dropdown list

--- 03 SpringMVCDemo/src/com/luv2code/springdemo/mvc/Student.java
+++ 03 SpringMVCDemo/src/com/luv2code/springdemo/mvc/Student.java
@@ -1,11 +1,22 @@
 package com.luv2code.springdemo.mvc;
 
+import java.util.LinkedHashMap;
+
 public class Student {
 
 	private String firstName;
 	private String lastName;
+	private String country;
+	private LinkedHashMap<String, String> countryOptions;
 
 	public Student() {
+		countryOptions = new LinkedHashMap<>();
+
+		countryOptions.put("EGY", "Egypt");
+		countryOptions.put("UAE", "Emirates");
+		countryOptions.put("DEU", "Germany");
+		countryOptions.put("ENG", "England");
+		countryOptions.put("FRA", "France");
 	}
 
 	public void setFirstName(String firstName) {
@@ -16,12 +27,24 @@ public class Student {
 		this.lastName = lastName;
 	}
 
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
 	public String getFirstName() {
 		return firstName;
 	}
 
 	public String getLastName() {
 		return lastName;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public LinkedHashMap<String, String> getCountryOptions() {
+		return countryOptions;
 	}
 
 }
